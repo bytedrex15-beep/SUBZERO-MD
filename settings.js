995c95a53f96dcf85b89c90e00739ddebea38820
bytedrex15-beep <bytedrex15@gmail.com>
2026-03-01 12:37:45 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "Ice~PNdJnA", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "255743140476", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
